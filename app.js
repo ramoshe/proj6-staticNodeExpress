5b692dabd498871fc05fc91b2a0e78f94cf1d5ce
Rachel Sherard <rsherard@mac.com>
2021-09-04 04:59:26 -0400

    edit for Heroku deployment

--- app.js
+++ app.js
@@ -70,6 +70,6 @@ app.use((err, req, res, next) => {
 /**
  * Start server
  */
-app.listen(3000, () => {
+app.listen((process.env.PORT || 3000), () => {
     console.log('Server listening on port 3000');
 })
